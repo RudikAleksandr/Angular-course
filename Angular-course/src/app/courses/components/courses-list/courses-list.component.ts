7517f14a06741bd35b8581b99d60cbadc7228aa8
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2019-10-13 17:58:07 +0300

    Add interfaces for user and course

--- Angular-course/src/app/courses/components/courses-list/courses-list.component.ts
+++ Angular-course/src/app/courses/components/courses-list/courses-list.component.ts
@@ -7,41 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesListComponent implements OnInit {
   public coursesList: any[] = [
-    {
-      id: 1,
-      title: 'Courses 1. Name tag',
-      creationDate: new Date().toString(),
-      duration: 50,
-      description: 'fdgdfg dfg f gsf gfsg dsjf dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh flksh fsh klsdhfkh skh lskdhf skd fksd hfjkhs jkfh skdhfkjdhs fkh sdkfh skdh fskjh flkshf lkjsdh fjsh fjdsh  sdf gfd gfsd g kjdh fjh ljhd sakjlhajhlfdlkh aileh liewuh liuewh hew lkjah kjweh fjah fjah l'
-    },
-    {
-      id: 1,
-      title: 'Courses 1. Name tag',
-      creationDate: new Date().toString(),
-      duration: 50,
-      description: 'fdgdfg dfg f gsf gfsg dsjf dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh flksh fsh klsdhfkh skh lskdhf skd fksd hfjkhs jkfh skdhfkjdhs fkh sdkfh skdh fskjh flkshf lkjsdh fjsh fjdsh  sdf gfd gfsd g kjdh fjh ljhd sakjlhajhlfdlkh aileh liewuh liuewh hew lkjah kjweh fjah fjah l'
-    },
-    {
-      id: 1,
-      title: 'Courses 1. Name tag',
-      creationDate: new Date().toString(),
-      duration: 50,
-      description: 'fdgdfg dfg f gsf gfsg dsjf dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh flksh fsh klsdhfkh skh lskdhf skd fksd hfjkhs jkfh skdhfkjdhs fkh sdkfh skdh fskjh flkshf lkjsdh fjsh fjdsh  sdf gfd gfsd g kjdh fjh ljhd sakjlhajhlfdlkh aileh liewuh liuewh hew lkjah kjweh fjah fjah l'
-    },
-    {
-      id: 1,
-      title: 'Courses 1. Name tag',
-      creationDate: new Date().toString(),
-      duration: 50,
-      description: 'fdgdfg dfg f gsf gfsg dsjf dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh flksh fsh klsdhfkh skh lskdhf skd fksd hfjkhs jkfh skdhfkjdhs fkh sdkfh skdh fskjh flkshf lkjsdh fjsh fjdsh  sdf gfd gfsd g kjdh fjh ljhd sakjlhajhlfdlkh aileh liewuh liuewh hew lkjah kjweh fjah fjah l'
-    },
-    {
-      id: 1,
-      title: 'Courses 1. Name tag',
-      creationDate: new Date().toString(),
-      duration: 50,
-      description: 'fdgdfg dfg f gsf gfsg dsjf dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh dsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldhdsljh fjkhsd fdjkh lfkh jkah fkjldh flksh fsh klsdhfkh skh lskdhf skd fksd hfjkhs jkfh skdhfkjdhs fkh sdkfh skdh fskjh flkshf lkjsdh fjsh fjdsh  sdf gfd gfsd g kjdh fjh ljhd sakjlhajhlfdlkh aileh liewuh liuewh hew lkjah kjweh fjah fjah l'
-    }
+
   ];
 
   constructor() { }
